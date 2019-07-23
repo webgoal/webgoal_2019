d4afab2951cfd215090b5c3e85bf8cb2238b661e
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-07-23 14:57:41 -0300

    navbar with mobile links

---
+++ components/navbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <nav>
+    <a
+      v-for="link in navbarList"
+      :key="link.name"
+      :href="link.url"
+      :class="link.classes"
+    >
+      {{ link.name }}
+    </a>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      navbarList: [
+        {
+          name: 'Home',
+          url: '#',
+          classes: 'sm:inline-block'
+        },
+        {
+          name: 'Negócios',
+          url: '#',
+          classes: 'hidden md:inline-block lg-inline-block xl-inline-block'
+        },
+        {
+          name: 'Experiências',
+          url: '#',
+          classes: 'hidden md:inline-block lg-inline-block xl-inline-block'
+        },
+        {
+          name: 'História',
+          url: '#',
+          classes: 'hidden md:inline-block lg-inline-block xl-inline-block'
+        },
+        {
+          name: 'Blog',
+          url: '#',
+          classes: 'sm:inline-block'
+        },
+        {
+          name: 'Contato',
+          url: '#',
+          classes: 'sm:inline-block'
+        }
+      ]
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,19 @@
 <template>
-  <div class="container">
-    <div>
-      <h1 class="font-serif">
-        Webgoal
-      </h1>
-      <h2 class="subtitle">
-        Site oficial webgoal
-      </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+  <section>
+    <Navbar />
+    <div class="container">
+      <p>narf</p>
     </div>
-  </div>
+  </section>
 </template>
+
+<script>
+import Navbar from '~/components/navbar'
+
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
+}
+</script>
