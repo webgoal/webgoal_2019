1d2e58cbe04b28d1571435ca293edb3306fef9b2
Lucas Azevedo <lucas.azevedo@webgoal.com.br>
2019-07-23 13:57:12 -0300

    Add fonts

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,10 @@ export default {
         content: process.env.npm_package_description || ''
       }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://use.typekit.net/ohv6flx.css' }
+    ]
   },
   /*
    ** Customize the progress-bar color
