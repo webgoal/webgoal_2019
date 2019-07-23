2845bc66ad9fd898fc5d289c5ec9f76dfbd66261
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-07-23 15:33:49 -0300

    home hero

---
+++ components/HomeHero.vue
@@ -0,0 +1,20 @@
+<template>
+  <div
+    class="hero pt-24 min-h-screen bg-no-repeat bg-cover bg-center flex flex-col items-center justify-center px-5"
+  >
+    <img
+      src="~/assets/images/logo-white.png"
+      alt="Logo Webgoal"
+      class="w-auto"
+    />
+    <p class="font-serif leading-none text-white text-4xl max-w-xs">
+      Negócios que tornam pessoas melhores.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.hero {
+  background-image: url('~assets/images/hero/home-hero.jpg');
+}
+</style>
