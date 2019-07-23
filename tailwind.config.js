5f606772de50ab2faa483039fd0ba328b526d98b
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-07-23 14:03:25 -0300

    Add default foints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 module.exports = {
   theme: {
+    fontFamily: {
+      serif: ['museo-slab', 'sans-serif'],
+      sans: ['museo-sans', 'sans']
+    },
     extend: {}
   },
   variants: {}
