e59aa6049c7210ca2d7479325340701713a97671
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-07-23 15:13:37 -0300

    Header

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <header class="flex flex-col items-center container mx-auto px-2">
+    <Logo />
+    <Navbar />
+  </header>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+import Navbar from '~/components/Navbar'
+
+export default {
+  name: 'Header',
+  components: {
+    Logo,
+    Navbar
+  }
+}
+</script>
