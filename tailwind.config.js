6f440738bb0b65424ad345d155084adf91da1a65
Atelie <atelie@iMac-de-Atelie.local>
2019-07-24 11:54:26 -0300

    add some layout configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 // See default config https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   theme: {
     fontFamily: {
-      serif: ['museo-slab', 'sans-serif'],
-      sans: ['museo-sans', 'sans']
+      serif: ['museo-slab', ...defaultTheme.fontFamily.serif],
+      sans: ['museo-sans', ...defaultTheme.fontFamily.sans]
     },
-    extend: {}
+    fontSize: { ...defaultTheme.fontSize, '7xl': '5rem' },
+    extend: {},
+    container: {
+      center: true,
+      padding: '1rem'
+    }
   },
   variants: {}
 }
