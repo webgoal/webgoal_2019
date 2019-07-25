0950bc64f87cc52186036cc009eb2315c737f569
davimattos <davi@webgoal.com.br>
2019-07-25 10:05:41 -0300

    add card component

---
+++ components/Card.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="container">
+    <Title :small-title="smallTitle" :title="title" />
+    <SubTitle :sub-title="subTitle" />
+    <Content :content="content" />
+    <Button :text="buttonText" />
+  </div>
+</template>
+<script>
+import Title from '~/components/Title'
+import SubTitle from '~/components/SubTitle'
+import Content from '~/components/Content'
+import Button from '~/components/Button'
+
+export default {
+  name: 'Card',
+  components: {
+    Title,
+    SubTitle,
+    Content,
+    Button
+  },
+  props: {
+    smallTitle: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    },
+    subTitle: {
+      type: String,
+      default: ''
+    },
+    content: {
+      type: String,
+      default: ''
+    },
+    buttonText: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,34 +2,26 @@
   <section>
     <Header />
     <HomeHero />
-    <div class="container">
-      <Title sub-title="estilo de" title="gestão" />
-      <SubTitle sub-title="Gestão centrada nas pessoas" />
-      <Content
-        content="Todas as empresas do grupo Webgoal adotam uma gestão orgânica, baseada na democracia e pautada em relações de confiança entre as pessoas. Hierarquias formais, comando e controle são substituídos por autonomia, colaboração e liderança emergente."
-      />
-      <Button text="saiba mais" />
-    </div>
+    <Card
+      small-title="estilo de"
+      title="gestão"
+      sub-title="Gestão centrada nas pessoas."
+      content="Todas as empresas do grupo Webgoal adotam uma gestão orgânica, baseada na democracia e pautada em relações de confiança entre as pessoas. Hierarquias formais, comando e controle são substituídos por autonomia, colaboração e liderança emergente."
+      button-text="saiba mais"
+    />
   </section>
 </template>
 
 <script>
 import Header from '~/components/Header'
 import HomeHero from '~/components/HomeHero'
-import Title from '~/components/Title'
-import SubTitle from '~/components/SubTitle'
-import Content from '~/components/Content'
-import Button from '~/components/Button'
-
+import Card from '~/components/Card'
 export default {
   name: 'Home',
   components: {
     Header,
     HomeHero,
-    Title,
-    SubTitle,
-    Content,
-    Button
+    Card
   }
 }
 </script>
