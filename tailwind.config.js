77e7b5ad32ba0f1a7133d96670c1ba9a0c51296e
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-08-14 17:35:00 -0300

    Add gray color

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,8 @@ module.exports = {
         black: '#000000',
         yellow: '#febd32',
         lightBlue: '#00b8d7',
-        lightGray: '#f8f8f8'
+        lightGray: '#f8f8f8',
+        gray: '#9f9f9f'
       }
     },
     container: {
