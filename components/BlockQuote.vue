0e7cadd7a9befb1ecff5e3a74371841d78e5ef4c
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-08-18 19:01:55 -0300

    blockquote

---
+++ components/BlockQuote.vue
@@ -0,0 +1,20 @@
+<template>
+  <blockquote class="relative my-4">
+    <span class="p-8 inline-block text-gray">
+      {{ quote }}
+    </span>
+    <img class="absolute top-0 left-0" src="~/assets/icons/quote.png" alt="">
+  </blockquote>
+</template>
+
+<script>
+export default {
+  name: 'Blockquote',
+  props: {
+    quote: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ pages/equipe.vue
@@ -0,0 +1,30 @@
+<template>
+  <section>
+    <Header dark :absolute="false" />
+    <div class="container">
+      <div class="flex flex-col">
+        <div class="w-full">
+          <Title small-title="conheça nossa" title="EQUIPE" />
+        </div>
+        <div class="w-full">
+          <BlockQuote quote="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna." />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Header from '~/components/Header'
+import Title from '~/components/Title'
+import BlockQuote from '~/components/BlockQuote'
+
+export default {
+  name: 'Equipe',
+  components: {
+    Header,
+    Title,
+    BlockQuote
+  }
+}
+</script>
