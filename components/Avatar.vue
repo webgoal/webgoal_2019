4c9ab0cdebc91b839632edfc6a096d15094f1b8c
Daniel Oshiro <daniel.oshiro@webgoal.com.br>
2019-08-19 19:28:11 -0300

    Avatar list

---
+++ components/Avatar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="w-full mb-8 relative text-center flex justify-center items-center">
+    <img :src="image" alt="">
+    <div class="absolute left-0 right-0 top-0 bottom-0 flex flex-col justify-end pb-10">
+      <p class="text-white font-bold">
+        {{ title }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Avatar',
+  props: {
+    image: {
+      type: String,
+      default: '//placehold.it/264x429'
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
--- pages/equipe.vue
+++ pages/equipe.vue
@@ -11,6 +11,7 @@
         </div>
       </div>
     </div>
+
     <div class="container">
       <div class="flex flex-row flex-wrap -mx-4">
         <div class="w-full md:w-4/12 px-4">
@@ -24,6 +25,15 @@
         </div>
       </div>
     </div>
+
+    <div class="container">
+      <div class="flex flex-row flex-wrap -mx-4">
+        <div v-for="(gridItem, index) in grid" :key="index" class="w-full px-4 flex justify-center items-center" :class="gridItem.size">
+          <Avatar v-if="gridItem.type === 'image'" :image="gridItem.image" :title="gridItem.title" />
+          <BlockQuote v-if="gridItem.type === 'blockquote'" :quote="gridItem.content" />
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -32,6 +42,7 @@ import Header from '~/components/Header'
 import Title from '~/components/Title'
 import BlockQuote from '~/components/BlockQuote'
 import IconCard from '~/components/IconCard'
+import Avatar from '~/components/Avatar'
 
 export default {
   name: 'Equipe',
@@ -39,7 +50,276 @@ export default {
     Header,
     Title,
     BlockQuote,
-    IconCard
+    IconCard,
+    Avatar
+  },
+  data() {
+    return {
+      grid: [
+        {
+          type: 'image',
+          title: 'Altieres',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Bruno',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Cainã',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Caio',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Canedo',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Cris',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Davi',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Dobrado',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Evelyn',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'blockquote',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+          image: '//placehold.it/264x429',
+          size: 'md:w-6/12'
+        },
+        {
+          type: 'image',
+          title: 'Fernanda',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Herbertt',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Igor',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Igor',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'João Paulo',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'blockquote',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Jorge',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Kim',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Lara',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Leo',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Leo',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Lucas',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Lucas',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Bruno',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Marly',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Mateus',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Matheus',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Miguel',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Paty',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'blockquote',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+          image: '//placehold.it/264x429',
+          size: 'md:w-6/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Peterson',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Raphael',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Rodrigo',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Sander',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Symone',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'blockquote',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Tiago',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Tom',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+
+        {
+          type: 'image',
+          title: 'Valter',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'Wallison',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'image',
+          title: 'William',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        },
+        {
+          type: 'blockquote',
+          content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna',
+          image: '//placehold.it/264x429',
+          size: 'md:w-3/12'
+        }
+      ]
+    }
   }
 }
 </script>
