f64ed84782b8e928ad41bcbe615b85eadef98b2a
Mateus Andrade <mateus.souza@webgoal.com.br>
2019-08-26 11:44:45 -0300

    inicio da pagina de historia

---
+++ pages/historia.vue
@@ -0,0 +1,27 @@
+<template>
+  <section>
+    <Header dark :absolute="false" />
+    <div class="container">
+      {{ nome }}
+    </div>
+    <Footer />
+  </section>
+</template>
+
+<script>
+import Header from '~/components/Header'
+import Footer from '~/components/Footer'
+
+export default {
+  name: 'Historia',
+  components: {
+    Header,
+    Footer
+  },
+  data() {
+    return {
+      nome: 'bla'
+    }
+  }
+}
+</script>
