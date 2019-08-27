26ac7421a722743280df0706d6b3a335e106325e
Full Name <hercrof@gmail.com>
2019-08-27 15:26:11 -0300

    Arruma margens da logo do granatum

--- components/Companies/Granatum.vue
+++ components/Companies/Granatum.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <div class="container mx-auto flex flex-col p-10 md:flex-row">
+    <div class="container mx-auto flex flex-col px-10 pb-3 lg:py-8 md:flex-row">
       <img class="lg:ml-10 my-auto mx-auto sm:mx-auto" src="~/assets/images/logo-granatum.svg" alt="">
       <div class="text-left text-l my-auto py-3 md:py-0 md:pl-10 md:pr-4 sm:px-auto">
         Serviço online de gestão financeira sem complicação que proporciona inteligência financeira para empresas e startups.
       </div>
       <Button class="m-auto" text="Saiba Mais" />
     </div>
-    <img class="mx-auto my-10 p-3 lg:p-0" src="~/assets/images/working.png" alt="">
+    <img class="mx-auto p-3" src="~/assets/images/working.png" alt="">
   </div>
 </template>
 
