a3a0a74a90aa511dac024d5d2b320324d8915394
Full Name <hercrof@gmail.com>
2019-08-27 15:13:59 -0300

    torna componente floz responsivo

--- components/Companies/Floz.vue
+++ components/Companies/Floz.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="bg-lightGray justify-center mx-auto p-10 flex flex-row bg-gray-300">
-    <div class="w-2/5 flex flex-col mr-16 my-auto justify-center">
-      <img class="mx-auto" src="~/assets/images/logo-floz.svg" alt="">
-      <div class="text-center p-auto text-l my-10 mx-auto">
+  <div class="bg-lightGray justify-center mx-auto p-10 flex flex-col-reverse lg:flex-row-reverse">
+    <img class="mx-auto lg:mx-10" src="~/assets/images/Floz.png" alt="">
+    <div class="flex flex-col my-auto pb-6 items-center">
+      <img src="~/assets/images/logo-floz.svg" class="mx-auto w-56 lg:mx-0" alt="">
+      <div class="text-center mx-auto lg:text-left text-l my-10">
         Startup de autosserviço para servir chopp e conectar pessoas, fazendo a gestão do estoque e também dos pagamentos.
       </div>
-      <Button class="text-l mx-auto" text="Saiba Mais" />
+      <Button class="m-auto text-left text-l" text="Saiba Mais" />
     </div>
-    <img class="items-center" src="~/assets/images/Floz.png" alt="">
   </div>
 </template>
 
