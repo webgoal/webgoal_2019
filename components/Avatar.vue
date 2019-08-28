0eefa7994548127d9cd8a4fa962dd68c7dec7847
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-08-28 09:38:35 -0300

    Melhoria na responsividade do titulo dos nomes da equipe

--- components/Avatar.vue
+++ components/Avatar.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="w-full mb-8 relative text-center flex justify-center items-center" @mouseover="show = true" @mouseleave=" show = false">
-    <img :src="image" alt="">
-    <div class="absolute left-0 right-0 top-0 bottom-0 flex flex-col justify-end -mb-3">
+    <div class="flex flex-col justify-end">
+      <img :src="image" alt="">
       <transition name="fade">
-        <div v-show="show" class="text-white font-bold bg-lightBlue m-3 text-xl">
+        <div v-show="show" class="title">
           {{ title }}
         </div>
       </transition>
@@ -37,5 +37,13 @@ export default {
   }
   .fade-enter, .fade-leave-to {
     opacity: 0;
+  }
+  .title {
+    height: 2rem;
+    color: white;
+    font-weight: bold;
+    background: #00b8d7;
+    font-size: 20px;
+    margin-top: -2rem;
   }
 </style>
