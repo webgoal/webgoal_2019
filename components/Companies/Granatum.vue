37d7d596b2245d85fa16522ee47a32a757333e14
Elton Fonseca <eltonjuniorfonseca@gmail.com>
2019-09-10 17:12:52 -0300

    Ajuste na responsividade e alinhamento

--- components/Companies/Granatum.vue
+++ components/Companies/Granatum.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-lightGray flex flex-col p-10 lg:flex-row justify-center">
-    <div class="container flex flex-col px-0 my-auto pb-6 lg:pb-0 lg:w-3/12 lg:m-0 items-end">
+    <div class="container flex flex-col px-0 my-auto pb-6 lg:pb-0 lg:w-3/12 lg:m-0 items-center lg:items-end">
       <img class="mx-auto w-56 lg:mx-0" src="~/assets/images/logo-granatum.svg" alt="">
       <div class="text-center pr-0 my-10 w-64 lg:text-right">
         Serviço online de gestão financeira sem complicação que proporciona inteligência financeira para empresas e startups.
